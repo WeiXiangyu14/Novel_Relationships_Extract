b7358d3385e3fda9c4b2a1043267d6b02fa391fa
yunhaoli <yunhaoli.cs@gmail.com>
2019-04-12 11:50:09 -0400

    Init -- LYH

--- RelationExtract.py
+++ RelationExtract.py
@@ -2,8 +2,17 @@ import nltk
 
 
 class RelationExtract:
-    def __init__(self):
+    def __init__(self, path="./Corpus/Harry Potter and the Sorcerer's Stone.txt"):
+        self.f = open(path, 'r')
+        self.name_list = self.get_name_list()
+
+    def extract_sentence(self):
         pass
+
+    def get_name_list(self):
+        f = open("./Corpus/namelist.txt")
+        names = f.read().split('\n')
+        return [name for name in names if len(name) > 0]
 
     def main(self):
         pass
