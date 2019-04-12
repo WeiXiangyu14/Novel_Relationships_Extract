95516511e9d4c89571815a114958f81bbcb78889
yunhaoli <yunhaoli.cs@gmail.com>
2019-04-12 11:26:14 -0400

    Init -- LYH

---
+++ RelationExtract.py
@@ -0,0 +1,14 @@
+import nltk
+
+
+class RelationExtract:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    re = RelationExtract()
+    re.main()
