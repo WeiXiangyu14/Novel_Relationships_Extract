4d0f7196a564ca6083ba40ed793cf1580850ac95
yunhaoli <yunhaoli.cs@gmail.com>
2019-04-12 12:09:38 -0400

    LYH: Splitting sentences.

--- RelationExtract.py
+++ RelationExtract.py
@@ -1,21 +1,31 @@
 import nltk
+# nltk.download('punkt')
 
 
 class RelationExtract:
     def __init__(self, path="./Corpus/Harry Potter and the Sorcerer's Stone.txt"):
-        self.f = open(path, 'r')
+        self.path = path
         self.name_list = self.get_name_list()
+        self.sentences = self.extract_sentence(self.path)
 
-    def extract_sentence(self):
-        pass
+    @staticmethod
+    def extract_sentence(path):
+        f = open(path, 'r')
+        text = f.read().replace("\n", " ")
+        print(len(text))
+        f.close()
+        sentences = nltk.tokenize.sent_tokenize(text)
+        return sentences
 
-    def get_name_list(self):
+    @staticmethod
+    def get_name_list():
         f = open("./Corpus/namelist.txt")
         names = f.read().split('\n')
         return [name for name in names if len(name) > 0]
 
     def main(self):
-        pass
+        print(len(self.sentences))
+        print(self.sentences)
 
 
 if __name__ == '__main__':
