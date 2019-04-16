df03095b226c9881592730b22983367ebfcfb396
XiangyuWei <weixy14@gmail.com>
2019-04-16 10:33:02 -0400

    naive handling "" and check dialog

--- pron_replace.py
+++ pron_replace.py
@@ -12,8 +12,14 @@ class NovelAnalyzer:
     current_person = ""
     current_people = []
     MAX_PEOPLE_NUM = 4
+    print_log = True
 
     end_token = [".", "!", "?"]
+    out_dialog_token = ["said"]
+    first_pron_token = ["i", "me", "we", "us"]
+    second_pron_token = ["you"]
+    third_people_token = ["they", "them"]
+    third_person_token = ["he", "she", "him", "her"]
 
     def __init__(self, filename):
         self.filename = filename
@@ -29,6 +35,7 @@ class NovelAnalyzer:
         texts = f.read()
         texts = texts.replace("\n", " ")
         texts = texts.replace("\r", " ")
+        texts = texts.replace("\"\"", "\" \"")
         doc = self.nlp(texts)
         for s in doc.sents:
             self.sentences_origin.append(s)
@@ -39,14 +46,15 @@ class NovelAnalyzer:
         fout = open("pron_sentences.txt", "w")
         i = 0
         in_dialog = False
+        last_token_text = ""
         if mode == "pron":
             for s in self.sentences_origin:
                 i += 1
                 if i % 100 == 0:
                     print(i)
 
-                if in_dialog:
-                    in_dialog = False
+                # if in_dialog:
+                #     in_dialog = False
 
                 # s_doc = self.nlp(s.string)
                 s_doc = s.as_doc()
@@ -59,41 +67,45 @@ class NovelAnalyzer:
                         # self.current_people.put(e.text)
                         # self.current_person = e.text
 
-
-                    # print(e.text, e.label_)
-                print()
-                print(s_people)
-                print(self.current_people)
-                print()
+                if self.print_log:
+                    print()
+                    print(s_people)
+                    print(self.current_people)
+                    print()
 
 
                 token_loc = 0
                 s_people_idx = 0
                 for token in s_doc:
+                    if token.lower_ in self.out_dialog_token:
+                        in_dialog = False
                     if token.tag_ == "\'\'":
                         in_dialog = not in_dialog
-                    if in_dialog:
-                        print("d: ", end="")
+                        # if last_token_text in self.end_token:
+                        #     in_dialog = False
+                    last_token_text = token.text
+
+                    if self.print_log:
+                        if in_dialog:
+                            print("d: ", end="")
                     if len(s_people) > s_people_idx:
                         if token_loc >= s_people[s_people_idx][1]:
-                            # print()
-                            # print(s_people[s_people_idx])
-                            # print()
                             person_name = s_people[s_people_idx][0]
                             self.add_person(person_name)
                             s_people_idx += 1
 
-                    print(token.text, token.tag_, token_loc)
+                    if self.print_log:
+                        print(token.text, token.tag_, token_loc)
                     fout.write(token.text)
                     if not in_dialog:
                         if token.tag_ == "PRP":
-                            print("FIND PRP")
+                            # print("FIND PRP")
                             fout.write("|||")
                             # if not self.current_people.empty():
                             #     fout.write(self.current_people.get())
                             #     fout.write("|||")
 
-                            if token.lower_ == "they" or token.lower_ == "them":
+                            if token.lower_ in self.third_people_token:
                                 allpeople = ""
                                 for pn in self.current_people:
                                     if not pn == "":
@@ -104,16 +116,25 @@ class NovelAnalyzer:
                                     fout.write("|||")
                                 else:
                                     fout.write("UNKNOWN_PEOPLE_NAMES|||")
-                            else:
+                            elif token.lower_ in self.third_person_token:
                                 if not self.current_person == "":
                                     fout.write(self.current_person)
                                     fout.write("|||")
                                 else:
                                     fout.write("UNKNOWN_NAME|||")
+                            elif token.lower_ in self.first_pron_token:
+                                # TODO: check whether "I" is in a dialog, process first pron
+                                fout.write("I_OUTSIDE_DIALOG")
+                                fout.write("|||")
+                            elif token.lower_ in self.second_pron_token:
+                                # TODO: check whether "you" is in a dialog, process second pron
+                                fout.write("YOU_OUTSIDE_DIALOG")
+                                fout.write("|||")
                         fout.write(" ")
                     else:
                         # TODO: Dialog check
-                        None
+                        fout.write("[D]")
+                        fout.write(" ")
 
                     token_loc += len(token.text)
                     token_loc += 1
@@ -154,10 +175,12 @@ class NovelAnalyzer:
 
 if __name__ == '__main__':
     # filename = sys.argv[1]
-    filename = "test.txt"
+    # filename = "test.txt"
+    # filename = "HP1.txt"
+    filename = "Cpart.txt"
     pron_replacer = NovelAnalyzer(filename)
     pron_replacer.read_file()
-    # pron_replacer.output_origin_sentences("sentences.txt")
+    pron_replacer.output_origin_sentences("sentences.txt")
     pron_replacer.extract_impt_sentences("pron")
     # pron_replacer.replace_pronoun()
 
